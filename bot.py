63a586d7ddc50133e12fe8933ef50aab938bcd13
Rita <mar.kheifets@gmail.com>
2020-10-08 01:21:11 +0300

    added code

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
+from emoji import emojize
+from glob import glob
 import logging
-from random import randint
-
+from random import choice, randint
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import keys
+import settings
 
 
 logging.basicConfig(filename="bot.log", level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d '
@@ -13,13 +15,21 @@ logging.basicConfig(filename="bot.log", level=logging.INFO, format='%(asctime)s 
 def greet_user(update, context):
     print(update)
     print("Вызван /start")
-    update.message.reply_text("Hello User!")
+    smile = get_emoji()
+    update.message.reply_text(f"Hello User {smile}")
 
 
 def talk_with_user(update, context):
+    smile = get_emoji()
     text = update.message.text
     print(text)
-    update.message.reply_text(text)
+    update.message.reply_text(f'{text} {smile}')
+
+
+def get_emoji():
+    emoji_smile = choice(settings.USER_EMOJI)
+    emoji_smile = emojize(emoji_smile, use_aliases=True)
+    return emoji_smile
 
 
 def random_number(user_number):
@@ -48,12 +58,22 @@ def play_number(update, context):
     update.message.reply_text(message)
 
 
+def send_cat_picture(update, context):
+    print("Вызван /cat")
+    cat_images_list = glob('images/cat*.jp*g')
+    cat_image = choice(cat_images_list)
+
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_image, 'rb'))
+
+
 def main():
     updater = Updater(keys.API_KEY, use_context=True)
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("play", play_number))
+    dp.add_handler(CommandHandler( "cat", send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_with_user))
 
     logging.info("Bot started")
